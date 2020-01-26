7646bab579d424c487b5a56c4521615e7d983b54
Amir Gezal <58855239+amirghezal@users.noreply.github.com>
2020-01-26 19:37:53 +0300

    Update gitHubViewController.swift

--- gitHubViewController.swift
+++ gitHubViewController.swift
@@ -1,10 +1,15 @@
+Данный пример очень плох с точки зрения разработчика: мы не проверяем возможность отслеживания, мы не проверяем статус разрешения,
+не промисываем что делать в случае ручного изменения разрешения и тд. Этот пример просто занакомит нас с тем, как добавлять mapKit 
+в свой проект
+
+
 import UIKit
 import YandexMapKit
 import CoreLocation
 
 class gitHubViewController: UIViewController {
     
-    //setting LocationManager
+    //создаем менеджера локации 
     let locationManager = CLLocationManager()
     
     //mapView
@@ -13,13 +18,17 @@ class gitHubViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
+        //получаем разрешение на использование
         locationManager.requestWhenInUseAuthorization()
+        //начием отслеживать локацию
         locationManager.startUpdatingLocation()
     }
     
     //метод, перемещающий карту на указанные координаты
     func moveMap(_ latitude: Double,_ longitude: Double) {
+        //создаем точку
         let mapPoint = YMKPoint(latitude: latitude, longitude: longitude)
+        //перемещаем карту на указанную точку
         self.mapView.mapWindow.map.move(with: YMKCameraPosition(target: mapPoint, zoom: 17, azimuth: 0, tilt: 0))
     }
 }
@@ -32,7 +41,9 @@ extension gitHubViewController: CLLocationManagerDelegate {
         let latitude = currentLocation?.coordinate.latitude
         let longitude = currentLocation?.coordinate.longitude
         DispatchQueue.main.async {
-            self.locationManager.startUpdatingLocation()
+            //останавливаем отслеживание локации
+            self.locationManager.stopUpdatingLocation()
+            //двигаем карту на первые полученные координаты
             self.moveMap(latitude!, longitude!)
         }
     }
